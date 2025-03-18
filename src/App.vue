8c81bb87c47c97c914c37a6f2a75f065bffc8e7d
TSL <2962380651@qq.com>
2025-03-18 17:14:07 +0800

    02完成批量操作及优化

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
-import { ref } from 'vue';
+import { ElMessage, ElMessageBox } from 'element-plus';
+import { ref, watch } from 'vue';
+
 const todoList = ref([]);
 const inputValue = ref('');
+const selectedTodos = ref([]); // 用于存储选中的待办事项索引
+const isAllSelected = ref(false);
 
 const addTodo = () => {
   const trimmedValue = inputValue.value.trim();
@@ -10,40 +14,119 @@ const addTodo = () => {
       showClose: true,
       message: '请输入待办事项',
       type: 'warning'
-    })
+    });
     return;
   }
   if (todoList.value.includes(trimmedValue)) {
-    ElMessage({
+    ElMessageBox({
       showClose: true,
       message: '不能添加相同的内容',
       type: 'warning'
-    })
+    });
     return;
   }
   todoList.value.push(trimmedValue);
-  inputValue.value = ''; // 清空输入框
-}
+  inputValue.value = '';
+};
 
 const deleteTodo = (index) => {
-  todoList.value.splice(index, 1);
+  ElMessageBox.confirm('确定删除该待办事项吗？', '提示', {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type: 'warning',
+    roundButton: true,
+    draggable: true
+  }).then(() => {
+    todoList.value.splice(index, 1)
+    ElMessage({
+      type: 'success',
+      message: '删除成功!'
+    })
+  }).catch(() => {
+    ElMessage({
+      type: 'info',
+      message: '已取消删除'
+    });
+  });
 }
+
+
+const deleteSelectedTodos = () => {
+  if (selectedTodos.value.length === 0) {
+    ElMessage({
+      type: 'warning',
+      message: '请选择待办事项'
+    });
+    return;
+  }
+  ElMessageBox.confirm (
+    '确定删除选中的待办事项吗？',
+    '提示',
+    {
+      confirmButtonText: '确定',
+      cancelButtonText: '取消',
+      type: 'warning',
+      roundButton: true,
+    draggable: true
+    }
+  )
+  .then( () => {
+    todoList.value = todoList.value.filter((_, index) => !selectedTodos.value.includes(index));
+    selectedTodos.value = []; 
+    isAllSelected.value = false; 
+    ElMessage({
+      type: 'success',
+      message: '删除成功!'
+    });
+  })
+  .catch(() => {
+    ElMessage({
+      type: 'info',
+      message: '已取消删除'
+    });
+  });
+};
+
+const toggleSelectAll = () => {
+  if (isAllSelected.value) {
+    selectedTodos.value = todoList.value.map((_, index) => index);
+  } else {
+    selectedTodos.value = [];
+  }
+};
+
+watch(selectedTodos, (newSelected) => {
+  isAllSelected.value = newSelected.length === todoList.value.length && todoList.value.length > 0;
+});
 </script>
 
 <template>
   <div class="contain">
-    <div class="input">
-      <el-input
-        v-model="inputValue"
-        placeholder="请输入待办事项"
-        aria-placeholder="请输入待办事项"
-      ></el-input>
-      <el-button size="large" type="primary" circle @click="addTodo">添加</el-button>
+    <div class="top">
+      <div class="input">
+        <el-input
+          v-model="inputValue"
+          placeholder="请输入待办事项"
+          aria-placeholder="请输入待办事项"
+          @keyup.enter="addTodo"
+        ></el-input>
+        <el-button class="addbutton" size="large" type="primary"  @click="addTodo">添加事项</el-button>
+      </div>
+    <div class="select-all">
+      <el-checkbox  size="large" v-model="isAllSelected" border @change="toggleSelectAll">全选</el-checkbox>
+      <el-button class="deleteselected" size="large" type="danger"  @click="deleteSelectedTodos">删除选中</el-button>
+    </div>
     </div>
     <ul class="list">
       <li v-for="(todo, index) in todoList" :key="index" class="item">
+        <el-checkbox
+          v-model="selectedTodos"
+          border
+          size="large"
+          :label="index"
+        ></el-checkbox>
         {{ todo }}
-        <el-button type="danger" size="mini" @click="deleteTodo(index)">删除</el-button>
+        <el-button class="delete" type="danger" size="mini" @click="deleteTodo(index)">删除</el-button>
       </li>
     </ul>
   </div>
@@ -51,23 +134,38 @@ const deleteTodo = (index) => {
 
 <style scoped>
 .contain {
-  width:100%;
-  height: 500px;
-  background-image: url("./assets/bg.png");
-  padding-top:40px;
+  width:1362px;
+  height: 600px;
 }
+
+ .top {
+  background-color: aquamarine;
+  width: 100%;
+  padding: 20px 0 10px 0;
+ }
 
 .input {
   width: 800px;
   height: 50px;
   display: flex;
   gap: 10px;
-  margin:0 0 0 300px ;
+  margin: 0 0 0 300px;
+}
+
+.select-all {
+  width: 77px;
+  margin: 10px 0 10px 50px;
+
 }
 
 .list {
   list-style: none;
-  padding: 0;
+  margin-top:10px;
+  width: 1320px;
+  height: 100%;
+  border: 1px solid black;
+  background-image: url("./assets/bg.png");
+  position: relative;
 }
 
 .item {
@@ -79,6 +177,20 @@ const deleteTodo = (index) => {
   margin-top: 10px;
   background-color: #fff;
   border-radius: 5px;
+}
+
+.addbutton {
+  margin: 5px 0 0 15px;
+}
+
+.deleteselected {
+  position: absolute;
+  left: 85%;
+  top: 14%;
+}
+
+.delete {
+  margin-right: 110px;
 }
 
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import 'element-plus/dist/index.css';
 
 import App from './App.vue'
 
